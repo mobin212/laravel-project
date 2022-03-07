9ca968bd73b1244f838ca474ebd6b58749aeccc5
mobin212 <mobin.abdoli@gmail.com>
2022-03-07 21:39:31 +0330

    cart item and number of it

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Cart;
 use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+// use Illuminate\Contracts\Session\Session;
+// use Session; 
 
 class ProductController extends Controller
 {
@@ -13,20 +17,33 @@ class ProductController extends Controller
         $data = Product::all();
         return view('product',['products'=>$data]);
     }
+
+    
     function detail($id)
     {
         $data = Product::find($id);
         return view('detail',['product'=>$data]);
     }
+    
+    
     function addToCart(Request $req)
     {
         if($req->session()->has('user'))
         {
-            return 'hello';
+            $cart = new Cart;
+            $cart->user_id=$req->session()->get('user')['id'];
+            $cart->product_id=$req->product_id;
+            $cart->save();
+            return redirect('/');
         }
         else
         {
             return redirect('/login');
         }
+    }
+    static function cartItem()
+    {
+        $userId=Session::get('user')['id'];
+        return Cart::where('user_id',$userId)->count();
     }
 }
--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,33 +1,39 @@
+<?php
+  use App\Http\Controllers\ProductController;
+  $total= ProductController::cartItem();
+?>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item m">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item m">
-            <a class="nav-link" href="#">Orders</a>
-          </li>
-          <li class="nav-item dropdown m">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown
-            </a>
-          </li>
-          <li class="nav-item m">
-            <a class="nav-link disabled">Disabled</a>
-          </li>
-        </ul>
-        <form class="d-flex">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success" type="submit">Search</button>
-          {{-- <li class="nav-item"> --}}
-            <a class="nav-link" href="#">Cart(0)</a>
-          {{-- </li> --}}
-        </form>
-      </div>
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item m">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item m">
+                    <a class="nav-link" href="#">Orders</a>
+                </li>
+                <li class="nav-item dropdown m">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        Dropdown
+                    </a>
+                </li>
+                <li class="nav-item m">
+                    <a class="nav-link disabled">Disabled</a>
+                </li>
+            </ul>
+            <form class="d-flex">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+                {{-- <li class="nav-item"> --}}
+                <a class="nav-link" href="#">Cart({{ $total }})</a>
+                {{-- </li> --}}
+            </form>
+        </div>
     </div>
-  </nav>
+</nav>
